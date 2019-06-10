d3ebb62429f29b9577e5c0f43ed401a68a52c6f8
Hung Dep Trai <hungdv39@viettel.com.vn>
2019-06-10 11:30:44 +0000

    First commit

---
+++ JavaIntroductionComprehensive/src/Chapter11/Geometric.java
@@ -0,0 +1,15 @@
+package Chapter11;
+
+public class Geometric {
+	private String color = "white";
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
